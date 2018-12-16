fea02950da67a15bc6b06584dc64b165e90daff4
Kelvin Salton do Prado <kelvinpfw@gmail.com>
2018-12-16 19:46:53 -0200

    sql: separar tabelas sql em arquivos

--- sql/municipios.sql
+++ sql/municipios.sql
@@ -1,10 +1,3 @@
-CREATE TABLE estados(
-    codigo_uf INT NOT NULL,
-    uf VARCHAR(2) NOT NULL,
-    nome VARCHAR(100) NOT NULL,
-    PRIMARY KEY (codigo_uf)
-);
-
 CREATE TABLE municipios(
     codigo_ibge INT NOT NULL,
     nome VARCHAR(100) NOT NULL,
@@ -15,35 +8,6 @@ CREATE TABLE municipios(
     PRIMARY KEY (codigo_ibge),
     FOREIGN KEY (codigo_uf) REFERENCES estados (codigo_uf)
 );
-
-INSERT INTO estados VALUES
-(11, 'RO', 'Rondônia'),
-(12, 'AC', 'Acre'),
-(13, 'AM', 'Amazonas'),
-(14, 'RR', 'Roraima'),
-(15, 'PA', 'Pará'),
-(16, 'AP', 'Amapá'),
-(17, 'TO', 'Tocantins'),
-(21, 'MA', 'Maranhão'),
-(22, 'PI', 'Piauí'),
-(23, 'CE', 'Ceará'),
-(24, 'RN', 'Rio Grande do Norte'),
-(25, 'PB', 'Paraíba'),
-(26, 'PE', 'Pernambuco'),
-(27, 'AL', 'Alagoas'),
-(28, 'SE', 'Sergipe'),
-(29, 'BA', 'Bahia'),
-(31, 'MG', 'Minas Gerais'),
-(32, 'ES', 'Espírito Santo'),
-(33, 'RJ', 'Rio de Janeiro'),
-(35, 'SP', 'São Paulo'),
-(41, 'PR', 'Paraná'),
-(42, 'SC', 'Santa Catarina'),
-(43, 'RS', 'Rio Grande do Sul'),
-(50, 'MS', 'Mato Grosso do Sul'),
-(51, 'MT', 'Mato Grosso'),
-(52, 'GO', 'Goiás'),
-(53, 'DF', 'Distrito Federal');
 
 INSERT INTO municipios VALUES
 (1100015, 'Alta Floresta D''Oeste', -11.9283, -61.9953, 0, 11),
